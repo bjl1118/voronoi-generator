8ff7d790392bf49219d7ce29950859320b416164
Ben Leibowitz-Lord <benleibowitzlord@gmail.com>
2021-11-29 14:54:47 -0500

    Displays initial voronoi diagram

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import './App.css';
 import Canvas from './components/Canvas';
 
 function App() {
+
+  const widthFactor = 0.75;
+  const heightFactor = 0.75
+
   return (
     <div className="App">
-      <Canvas />
+      <Canvas height={window.innerHeight * heightFactor} width={window.innerWidth * widthFactor} />
     </div>
   );
 }
--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,21 +1,33 @@
 import React, { useRef, useEffect, useCallback } from "react";
+import { Delaunay } from 'd3-delaunay';
 
 const Canvas = props => {
+    const n = 200;
     const canvasRef = useRef(null);
+    const particles = Array.from({length: n}, () => [Math.random() * props.width, Math.random() * props.height]);
 
     const draw = useCallback(ctx => {
-        ctx.fillStyle = '#000000'
-        ctx.beginPath()
-        ctx.arc(50, 100, 20, 0, 2 * Math.PI)
-        ctx.fill()
-    }, [])
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+        ctx.fillStyle = '#ffffff';
+        ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+        const delaunay = Delaunay.from(particles);
+        const voronoi = delaunay.voronoi([0.5, 0.5, ctx.canvas.width, ctx.canvas.height]);
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+        ctx.beginPath();
+        voronoi.render(ctx);
+        voronoi.renderBounds(ctx);
+        ctx.strokeStyle = '#000';
+        ctx.stroke();
+    }, [particles]);
 
     useEffect(() => {
         const canvas = canvasRef.current
         const context = canvas.getContext('2d')
         
         draw(context)
-    }, [draw])
+    }, [draw]);
 
     return <canvas ref={canvasRef} {...props} />
 }
