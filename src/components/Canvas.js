30fc5f6f6e24df49297a07d5289ce869ad853550
Ben Leibowitz-Lord <benleibowitzlord@gmail.com>
2021-11-30 16:02:12 -0500

    Handles mouseover

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -2,18 +2,17 @@ import React, { useRef, useEffect, useCallback } from "react";
 import { Delaunay } from 'd3-delaunay';
 
 const Canvas = props => {
-    const n = 200;
+    const n = 50;
     const canvasRef = useRef(null);
-    const particles = Array.from({length: n}, () => [Math.random() * props.width, Math.random() * props.height]);
+    const particles = Array.from({ length: n }, () => [Math.random() * props.width, Math.random() * props.height]);
 
-    const draw = useCallback(ctx => {
+    const update = useCallback(ctx => {
         ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
         ctx.fillStyle = '#ffffff';
         ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
         const delaunay = Delaunay.from(particles);
         const voronoi = delaunay.voronoi([0.5, 0.5, ctx.canvas.width, ctx.canvas.height]);
-        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
         ctx.beginPath();
         voronoi.render(ctx);
@@ -22,12 +21,19 @@ const Canvas = props => {
         ctx.stroke();
     }, [particles]);
 
+    const handleMouseMove = useCallback((context, event) => {
+        particles[0] = [event.layerX, event.layerY];
+        update(context);
+    }, [particles, update]);
+
     useEffect(() => {
-        const canvas = canvasRef.current
-        const context = canvas.getContext('2d')
-        
-        draw(context)
-    }, [draw]);
+        const canvas = canvasRef.current;
+        const context = canvas.getContext('2d');
+        context.canvas.onmousemove = event => {
+            handleMouseMove(context, event);
+        };
+        update(context)
+    }, [handleMouseMove, update]);
 
     return <canvas ref={canvasRef} {...props} />
 }
