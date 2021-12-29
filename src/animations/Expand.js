1cee513b6e6a891251c794f9358ce3ac2c66c5de
Ben Leibowitz-Lord <benleibowitzlord@gmail.com>
2021-12-29 14:40:39 -0500

    Removes commented code

--- src/animations/Expand.js
+++ src/animations/Expand.js
@@ -9,23 +9,6 @@ const expand = (particles, index, ctx) => {
     const vector = normalize(centerPoint, p);
     let nextX = Math.round((p[0] + vector[0]) * 100) / 100;
     let nextY = Math.round((p[1] + vector[1]) * 100) / 100;
-    // if (
-    //     nextX < 0 ||
-    //     nextY < 0 ||
-    //     nextX > ctx.canvas.width ||
-    //     nextY > ctx.canvas.height
-    // ) {
-    //     const widthMax = ctx.canvas.width / 2 + 5;
-    //     const widthMin = ctx.canvas.width / 2 - 5;
-    //     const heightMax = ctx.canvas.height / 2 + 5;
-    //     const heightMin = ctx.canvas.height / 2 - 5;
-    //     nextX = Math.floor(
-    //         Math.random() * (widthMax - widthMin) + widthMin
-    //     );
-    //     nextY = Math.floor(
-    //         Math.random() * (heightMax - heightMin) + heightMin
-    //     );
-    // }
     return [nextX, nextY];
 }
 
