229a1e363e8baf1c2bead8b612d8484cf2e95fa6
Ben Leibowitz-Lord <benleibowitzlord@gmail.com>
2022-01-03 12:54:05 -0500

    Updates readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # voronoi-generator
 
+A voronoi diagram generator created with React and D3. Includes several customization options and animations to explore.
+
 [Github Pages Link](https://bjl1118.github.io/voronoi-generator)
--- src/animations/Orbit.js
+++ src/animations/Orbit.js
@@ -1,19 +1,21 @@
 import { distance, vector } from "../utils/utils";
 
-const orbit = (particles, index, ctx, frameCount = 0) => {
-    if (frameCount % 1 === 0) {
-        const centerPoint = [ctx.canvas.width / 2, ctx.canvas.height / 2];
-        const p = particles[index];
-        const radius = distance(p[0], p[1], centerPoint[0], centerPoint[1])
-        const v = vector(centerPoint, p);
-        const radians = Math.atan2(v[1], v[0]);
-        const newRadians = index % 2 === 0 ? radians + 0.003 : radians - 0.003;
-        const newX = centerPoint[0] + (radius * Math.cos(newRadians))
-        const newY = centerPoint[1] + (radius * Math.sin(newRadians))
-        return [newX, newY];
-    }
-    return particles[index];
+const orbit = (particles, index, ctx) => {
 
+    // The amount to move each point by, in radians
+    const radianDiff = 0.003
+
+    const centerPoint = [ctx.canvas.width / 2, ctx.canvas.height / 2];
+    const p = particles[index];
+    const radius = distance(p[0], p[1], centerPoint[0], centerPoint[1])
+    const v = vector(centerPoint, p);
+    const radians = Math.atan2(v[1], v[0]);
+
+    // Have half the points go clockwise, the others go counter clockwise
+    const newRadians = index % 2 === 0 ? radians + radianDiff : radians - radianDiff;
+    const newX = centerPoint[0] + (radius * Math.cos(newRadians))
+    const newY = centerPoint[1] + (radius * Math.sin(newRadians))
+    return [newX, newY];
 }
 
 export default orbit;
